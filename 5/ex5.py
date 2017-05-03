d1e1125ac500d36b33cd86338d9a48a0747a1a2c
Mustafa Öztürk <mail2musti@gmail.com>
2017-05-04 01:08:35 +0200

    ok

---
+++ 5/ex5.py
@@ -0,0 +1,16 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = "White"
+hair = "Brown"
+
+print("Let's talk about {}.".format(name))
+print("He's {} inches tall.".format(height))
+print("He's {} pounds heavy.".format(weight))
+print("Actually that's not too heavy.")
+print("He's got {} eyes and {} hair.".format(eyes, hair))
+print("His teeth are usually {} depending on the coffee.".format(teeth))
+
+print("If I add {}, {}, and {} I get {}.".format(age, height, weight, age + height + weight))
