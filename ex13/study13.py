97bd003503502f09b7cadef3cd6aacca916bd6d4
Mustafa Öztürk <mail2musti@gmail.com>
2017-05-07 01:43:27 +0200

    ok

---
+++ ex13/study13.py
@@ -0,0 +1,33 @@
+from sys import argv
+
+"""
+script, eins, zwei = argv
+
+print("scriptname:", script)
+print("1argument:", eins)
+print("2argument:", zwei)
+
+
+"""
+
+# break
+"""
+script, a, b, c, d, e = argv
+
+print("scriptname:", script)
+print("arguA:", a)
+print("arguB:", b)
+print("arguC ist {}.".format(c))
+print(d)
+print(float(e))
+"""
+
+
+script, test1, test2, test3 = argv
+print(test1)
+
+test1 = input("Repeat your input:")
+
+print(test1)
+print(test2)
+print(test3)
